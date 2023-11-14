20f952e79098a690c800dbe15ddb13a01ce469b0
PC08\Student <student@etfbl.net>
2023-11-14 14:32:22 +0100

    Dodajem funkciju koja vraca korijen dva broja

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+int kvadratni_korijen(int a, int b)
+{
+    return sqrt(a,b);
+}
 
 int main()
 {
-    printf("Hello world!\n");
+    printf("%d", kvadratni_korijen(2, 2));
     return 0;
 }
