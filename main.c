7d50dfaf417ee9fa3928b07ab16456d2dcbb045e
PC07\Student <student@etfbl.net>
2023-11-14 14:34:06 +0100

    mnozenje 2 broja

--- main.c
+++ main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int mnozenje()
+{
+    int a, b;
+    printf("a=");
+    scanf("%d", &a);
+    printf("b=");
+    scanf("%d", &b);
+    return a * b;
+}
 int main()
 {
     printf("Hello world!\n");
+    printf("Prozivod dva broja: %d", mnozenje());
     return 0;
 }
