20307afa5772729d4f645b79b58a194436634987
PC10\Student <student@etfbl.net>
2023-11-14 14:41:01 +0100

    oduzimanje 2 broja

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int oduzimanje_dva_br(int a, int b){
+    printf("Unesite prvi broj:" ); scanf("%d", &a);
+    printf("Unesite drugi broj:" ); scanf("%d", &b);
+   return a - b;
+}
+
 int main()
 {
-    printf("Hello world!\n");
+    int a, b;
+   printf("%d", oduzimanje_dva_br(a, b));
     return 0;
 }
