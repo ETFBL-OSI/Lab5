5acfefcc441cfe4baa06a365fbb6d2e2636b573c
PC03\Student <studentetfbl@etf.bl.net>
2023-11-14 14:32:28 +0100

    funkcija za dijeljenje dva broja

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+double dijeljenjeDvaBroja (double a, double b)
+{
+    if (b==0)
+        return;
+    else
+        return a/b;
+}
+
 int main()
 {
-    printf("Hello world!\n");
+    double a, b;
+
+    scanf("%lf %lf", &a, &b);
+
+    printf("Kolicnik je: %lf", dijeljenjeDvaBroja(a,b));
+
     return 0;
 }
