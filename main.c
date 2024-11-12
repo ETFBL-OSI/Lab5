31272660931456dc87816bbb830b0c44db44cac1
PC-21\Student <student@etf.bl.net>
2024-11-12 14:37:25 +0100

    Dijeljenje 2 broja

--- main.c
+++ main.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 
+double dijeljenjeDvaBroja(double a, double b)
+{
+    if(b==0)
+    {
+        printf("Greska!Dijeljenje sa 0.");
+        return 0;
+    }
+        printf("%lf",a/b);
+    return a/b;
+}
 
 int main()
 {
-
-
+    dijeljenjeDvaBroja(7,5);
     return 0;
 }
