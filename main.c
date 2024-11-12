6f95d3749874a63a5be2c2ed26e7ab61df482630
PC16\Student <student@etfbl.net>
2024-11-12 14:40:45 +0100

    oduzimanje 2 broja

--- main.c
+++ main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
+int oduzimanje(int broj1, int broj2)
+{
+    int rez=broj1-broj2;
+    return rez;
+}
 
 int main()
 {
+    int broj1, broj2;
+    printf("unesi 1.broj: ");
+    scanf("%d", &broj1);
+    printf("unesi 2.broj: ");
+    scanf("%d", &broj2);
+    if(broj1>broj2)
+    {
+    printf ("%d",oduzimanje(broj1, broj2));
+    }
+    else
+        printf("greska");
 
 
     return 0;
