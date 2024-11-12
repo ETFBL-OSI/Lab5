1c172fc92402cee903201e69645ea8ba889ede9e
PC14\Student <studentetfbl.net>
2024-11-12 14:44:21 +0100

    sabiranje 2 broja

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
-
+int zbir2Broja(int a, int b)
+{
+    return a + b;
+}
 int main()
 {
-
-
+    int a, b;
+    scanf("%d %d", &a, &b);
+    int rez;
+    rez = zbir2Broja(a, b);
+    printf("rezultat je: %d", rez);
     return 0;
 }
