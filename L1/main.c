17ac5c22405b29e72bb2ad50961f453e4a3a51f8
PC16\Student <student@etfbl.net>
2025-11-10 12:37:06 +0100

    Dodano mnozenje 2 broja.

--- L1/main.c
+++ L1/main.c
@@ -11,3 +11,15 @@ void sum_of_two() {
 
     printf("a + b = %lf", a + b);
 }
+
+void proizvod_2_broja()
+{
+    double a, b;
+    printf("a = ");
+    scanf("%lf", &a);
+
+    printf("b = ");
+    scanf("%lf", &b);
+    printf("a*b = %lf", a*b);
+}
+
