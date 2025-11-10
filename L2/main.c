b6cc234c37bbd9345606b48f3a89ba2ac13e412c
PC17\Student <student@etf.bl.net>
2025-11-10 13:37:54 +0100

    kkk

--- L2/main.c
+++ L2/main.c
@@ -2,16 +2,15 @@
 <<<<<<< HEAD
 int sumaKvadrata(int a, int b) {
     return a*a + b*b;
-=======
+}
 int racunanjeFaktorijela(int n)
 {   
     int p=n;
     for(int i=n-1; i>1; i--)
         p*=i;
     return p;
->>>>>>> bdf19a204cede1a1b93ee2d1c2063e661d383349
 }
-=======
+
 //najveci od n brojeva
 
 int max(int niz[n],int n){
@@ -22,4 +21,4 @@ for(int i=0;i<n;i++){
 }
 return max;
 }
->>>>>>> 7542a4cde6d1bd5a8ae456fb014e08516b4ff56f
+
