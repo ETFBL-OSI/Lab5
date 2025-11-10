f4fb8018c1f9c504792f6fc34eadb2c6a5da9e47
PC04\Student <studentemail@email.com>
2025-11-10 14:44:07 +0100

    Operacija mnozenja

--- L3/main.c
+++ L3/main.c
@@ -1,13 +1,21 @@
 #include "stdio.h"
 
-int main() {
-int a, b, c;
+int mul(int a, int b) {
+int  c;
 printf("Unesite broj a:");
 scanf("%d", &a);
 printf("Unesite broj b:");
 scanf("%d", &b);
 c = a * b;
-printf("Rezultat: %d ", c);
+printf("Rezultat: %d", c);
+return c;
+}
+
+int main() {
+
+int a,b;
+mul(a,b);
+
 return 0;
 
 }
