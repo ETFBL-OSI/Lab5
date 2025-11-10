bfb8042df73717f562dd0ca9c642235b15ac36e7
PC04\Student <studentemail@email.com>
2025-11-10 14:33:37 +0100

    Kod sa operacijom mnozenja

---
+++ L3/main.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+
+int main() {
+int a, b, c;
+printf("Unesite broj a:");
+scanf("%d", &a);
+printf("Unesite broj b:");
+scanf("%d", &b);
+c = a * b;
+printf("Rezultat: %d ", c);
+return 0;
+
+}
