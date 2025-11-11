d453b106bf0b27a3fdf3133459a762e7da3d9966
PC03\Student <studentetfbl@etf.bl.net>
2025-11-11 13:36:42 +0100

    mnozenje dva broja

---
+++ L4/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int mnozenje()
+{
+    int a, b;
+
+    printf("Unesi a: ");
+    scanf("%d\n");
+
+    printf("Unesi b: ");
+    scanf("%d\n");
+
+    int proizvod = a*b;
+
+    return proizvod;
+}
