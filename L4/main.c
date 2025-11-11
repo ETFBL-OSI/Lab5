85fc022baaceae44dbd609f7e83edeecf58db0a3
PC08\Student <student@etfbl.net>
2025-11-11 13:43:56 +0100

    sabiranje 2 broja

---
+++ L4/main.c
@@ -0,0 +1,12 @@
+double sabiranje_2_broja()
+{
+    double a,b;
+    printf("Unesi prvi broj: ");
+    scanf("%lf\n", a);
+
+    printf("Unesi drugi broj: ");
+    scanf("%lf\n", b);
+
+    double zbir=a+b;
+    return zbir;
+}
