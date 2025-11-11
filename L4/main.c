4651357286e096aba396a9643ffa2515322d17c1
PC10\Student <student@etfbl.net>
2025-11-11 13:41:37 +0100

    funkcija za korijen broja

---
+++ L4/main.c
@@ -0,0 +1,11 @@
+int korijen_broja()
+{
+    double broj;
+    printf("unesite broj:");
+    scanf("%lf",&broj);
+
+    double rez;
+    rez=sqrt(broj);
+    printf("korijen broja je: %lf", korijen);
+    return 0;
+}
