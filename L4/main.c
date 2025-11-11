400e5583548c3b6e6c30663ca97badd9d91d0c9e
PC03\Student <studentetfbl@etf.bl.net>
2025-11-11 13:40:12 +0100

    mnozenje dva broja

--- L4/main.c
+++ L4/main.c
@@ -1,23 +1,9 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 int mnozenje()
 {
-    int a, b;
-
-    printf("Unesi a: ");
-    scanf("%d\n");
-
-    printf("Unesi b: ");
-    scanf("%d\n");
-
+    int a=5, b=3;
     int proizvod = a*b;
 
     return proizvod;
 }
-=======
-double procenat()
-{
-	return (1.00 / 5.00) * 100.00;
-}
->>>>>>> eddc45d9a72dcb4d969b9a944ef1af17767e138e
