ab2bd055d80d8d4daf43c1eab7ed5e15ac9d744f
PC16\Student <student@etfbl.net>
2025-11-11 13:48:29 +0100

    Stepenovanje

---
+++ L4/main.c
@@ -0,0 +1,12 @@
+double stpenovanje()
+{
+    double baza=3.0;
+    int eksponent=2;
+    double rezultat=1.0;
+    for(int i=0;i<eksponent;i++)
+    {
+        rezultat*=baza;
+    }
+    return rezultat;
+
+}
